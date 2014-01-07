85c801b0d9be0aa845c8d3883e6732b76e4483fc
miknatr <miknatr@gmail.com>
2014-01-08 02:39:55 +0400

    Экранирование в шаблонах

--- lib/Bravicility/Templater/Templater.php
+++ lib/Bravicility/Templater/Templater.php
@@ -16,11 +16,36 @@ class Templater
     {
         extract($vars);
         extract($this->commonVars);
+
+        // STOPPER какое-то не очень место (очень не очень)
+        // но если в шаблонах объявлять неймспейс, тогда там нельзя отключить подчеркивание необъявленных переменных (а так можно для глобального неймспеса)
+        // отключать подчеркивание необъявленных переменных для всего файла вроде нет способа (тогда можно для шаблонов бы тупо отключить)
+        // так тоже магия, но этот способ можно допилить до плагинов?
+        $e     = 'static::e';
+        $eAttr = 'static::eAttr';
+        $eUrl  = 'static::eUrl';
+
         ob_start();
         /** @noinspection PhpIncludeInspection */
         include $this->templateDir . $file . '.html.php';
         $r = ob_get_contents();
         ob_end_clean();
         return $r;
+    }
+
+    public static function e($value)
+    {
+        return htmlspecialchars($value);
+    }
+
+    public static function eAttr($value)
+    {
+        // STOPPER здесь че вще
+        return $value;
+    }
+
+    public static function eUrl($value)
+    {
+        return urlencode($value);
     }
 } 
