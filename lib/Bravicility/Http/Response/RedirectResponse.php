578d13108d0029906e24bd6a8db53ca8d1235fa6
Aleksandr Galkin <agalkin@agalkin.ru>
2014-01-24 15:33:55 +0400

    Нормальный RedirectResponse для урлов без хоста и схемы

--- lib/Bravicility/Http/Response/RedirectResponse.php
+++ lib/Bravicility/Http/Response/RedirectResponse.php
@@ -4,12 +4,37 @@ namespace Bravicility\Http\Response;
 
 class RedirectResponse extends Response
 {
+    protected $location;
+
     public function __construct($location)
     {
-        if (!preg_match('#^https?://#', $location)) {
-            throw new \LogicException('Location must be absolute');
+        if (!preg_match('#^https?://#', $location) && substr($location, 0, 1) != '/') {
+            throw new \LogicException('Redirect location must be absolute, "' . $location . '" is not');
         }
 
-        parent::__construct(302, array('Location: ' . $location));
+        parent::__construct(302);
+        $this->location = $location;
+    }
+
+    public function send()
+    {
+        $location = $this->location;
+
+        if (!preg_match('#^https?://#', $location)) {
+            $location = $this->getSchema() . '://' . $_SERVER['HTTP_HOST'] . $location;
+        }
+
+        $this->headers[] = 'Location: ' . $location;
+
+        parent::send();
+    }
+
+    private function getSchema()
+    {
+        if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) {
+            return 'https';
+        }
+
+        return 'http';
     }
 }
