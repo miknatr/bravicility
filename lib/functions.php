45006a0faaa219bd18d1ab62483dd6783ef4186f
miknatr <miknatr@gmail.com>
2014-08-08 16:18:31 +0400

    Дефисы

--- lib/functions.php
+++ lib/functions.php
@@ -32,7 +32,7 @@ function formatMoney($amount)
 function phoneToInt($phone)
 {
     $phone = preg_replace('/\D+/', '', $phone);
-    $phone = preg_replace('/^8\d{10}$/', '7$1$2-$3-$4', $phone);
+    $phone = preg_replace('/^8\d{10}$/', '7$1', $phone);
     return $phone;
 }
 
